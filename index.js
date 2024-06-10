f9e7c972593de0c51c86afded28dacf3f4711e34
mhsetu <mhsetu111@gmail.com>
2024-06-10 22:52:13 +0600

    sixth

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
